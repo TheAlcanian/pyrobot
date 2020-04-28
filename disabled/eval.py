d1ee77be4494e2270be0eb4928820f5d1a1ed1d1
TheAlcanian <IAmTheShardmaster@protonmail.com>
2020-04-27 20:57:51 -0700

    fuck disabled cmds lmao

--- disabled/eval.py
+++
@@ -1,6 +0,0 @@
-from discord.ext import commands
-import discord
-@bot.command()
-async def eval(ctx, arg):
-  varxc = compile(str(arg), '<string>', 'exec', flags=0, dont_inherit=False, optimize=-1)
-  await exec(varxc)
