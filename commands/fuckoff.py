14904fd5f8d923115ca89e4e15f5c418aaee0cbc
TheAlcanian <IAmTheShardmaster@protonmail.com>
2020-04-29 12:44:18 -0700

    fix fuckoff

--- commands/fuckoff.py
+++ commands/fuckoff.py
@@ -1,16 +1,30 @@
 @bot.command()
 async def fuckoff(ctx):
-  embed = discord.Embed(title="Shutdown", description="Being the being of begone-ing")
 
-  embed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
-  # embed.add_field(name="Voice Latency", value=str(VoiceClient.latency))
-  embed.add_field(name="Fucking off...", value='<:hahayes:694385453624721438>')
+  if await bot.is_owner(ctx.message.author):
+    embed = discord.Embed(title="Shutdown", description="Being the being of begone-ing")
 
-  await ctx.send(embed=embed)
-  inventoryfile = open("./inventory.json", "r+")
-  currencyfile = open("./currency.json", 'r+')
-  json.dump(items, inventoryfile)
-  json.dump(amounts, currencyfile)
-  inventoryfile.close()
-  currencyfile.close()
-  exit(0)
+    embed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
+    # embed.add_field(name="Voice Latency", value=str(VoiceClient.latency))
+    embed.add_field(name="Fucking off...", value='<:hahayes:694385453624721438>')
+
+    await ctx.send(embed=embed)
+
+ # inventoryfile.close()
+ # currencyfile.close()
+
+    inventoryfile = open("./inventory.json", "w+")
+    currencyfile = open("./currency.json", 'w+')
+    json.dump(items, inventoryfile)
+    json.dump(amounts, currencyfile)
+    inventoryfile.close()
+    currencyfile.close()
+    exit(0)
+  else:
+    embed = discord.Embed(title="Shutdown", description="Being the being of begone-ing")
+
+    embed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
+    # embed.add_field(name="Voice Latency", value=str(VoiceClient.latency))
+    embed.add_field(name="Error", value='You aren\'t allowed to tell me to fuck off!')
+
+    await ctx.send(embed=embed)
--- commands/updatecurrency.py
+++ commands/updatecurrency.py
@@ -1,5 +1,5 @@
 @bot.command(name='updatecurrency', aliases=['updc'], brief='Updates the currency file as a test', pass_ctx=True)
 async def updatecurrency(ctx):
-  currencyfile = open("./currency.json", 'r+')
+  currencyfile = open("./currency.json", 'w+')
   json.dump(amounts, currencyfile)
   await ctx.send('Currency updated :hahayes:')
