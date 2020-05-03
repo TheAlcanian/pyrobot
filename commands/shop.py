81b7263f8d7102d219e6f37033086b33a99523e7
Shardion <IAmTheShardmaster@protonmail.com>
2020-05-03 12:49:26 -0700

    small fix to pr.shop

--- commands/shop.py
+++ commands/shop.py
@@ -1,6 +1,6 @@
 @bot.command()
 async def shop(ctx):
-  shopEmbed = discord.Embed(title="Shop", description="Buy something using `pr.buy ")
+  shopEmbed = discord.Embed(title="Shop", description="Buy something using `pr.buy`")
 
   shopEmbed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
   # embed.add_field(name="Voice Latency", value=str(VoiceClient.latency))
