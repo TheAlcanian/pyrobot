1b8996adc174e3f291f7abfd0401ed598b5de986
TheAlcanian <IAmTheShardmaster@protonmail.com>
2020-05-04 19:18:21 -0700

    fix #11 and eliminate pr.gamble from #8

--- commands/gamble.py
+++ commands/gamble.py
@@ -5,31 +5,51 @@ async def gamble(ctx, arg):
   badArgEmbed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
   
   if str.isdigit(arg):
-
+    currencyfile = open('./currency.json', 'r')
+    amounts = json.load(currencyfile)
+    currencyfile.close()
     integerargument = int(arg)
-    gainedcurrency = random.choice([integerargument * 2, 0])
+    if random.randint(1, 2) == 2:
+      gainedcurrency = int(arg) * 2
+    else:
+      gainedcurrency = 0
+
+    #money gain embed
     moneyGainedEmbed = discord.Embed(title="Gambling", description="Gambling away your PyRolls got you...")
     moneyGainedEmbed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
     moneyGainedEmbed.add_field(name=str(gainedcurrency) + ' PyRolls!', value='Don\'t spend it all on one thing!')
+    #money lost embed
     moneyLostEmbed = discord.Embed(title="Gambling", description="Gambling away your PyRolls got you...")
     moneyLostEmbed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
     moneyLostEmbed.add_field(name=str(gainedcurrency) + ' PyRolls...', value='Better luck next time!')
+    #dont have enough money embed
     noMoneyEmbed = discord.Embed(title="Gambling", description="You don't have enough PyRolls!")
     noMoneyEmbed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
-    
+
+
+    #if user doesnt have enough pyrolls to fufill the requirement for the bet they chose then ignore it and tell them they dont have enough
     currentuseramount = int(amounts[str(ctx.message.author.id)])
     if int(currentuseramount) <= int(int(arg) - 1):
       await ctx.send(embed=noMoneyEmbed)
       return
+
+    #debugging
     print(integerargument)
+    
     amountlost = currentuseramount - int(arg)
     amounts[str(ctx.message.author.id)] = int(amountlost)
     addamount = int(amounts[str(ctx.message.author.id)]) + gainedcurrency
     amounts[str(ctx.message.author.id)] = str(addamount)
+    currencyfile = open('./currency.json', 'w')
+    json.dump(amounts, currencyfile)
+    currencyfile.close()
     print(amounts)
     if gainedcurrency == 0:
-      amountlost = currentuseramount + int(arg)
+      amountlost = currentuseramount - int(arg)
       amounts[str(ctx.message.author.id)] = int(amountlost)
+      currencyfile = open('./currency.json', 'w')
+      json.dump(amounts, currencyfile)
+      currencyfile.close()
       await ctx.send(embed=moneyLostEmbed)
     else:
       await ctx.send(embed=moneyGainedEmbed)
--- commands/shop.py
+++ commands/shop.py
@@ -2,8 +2,31 @@
 async def shop(ctx):
   shopEmbed = discord.Embed(title="Shop", description="Buy something using `pr.buy`")
 
+  itemsfile = open("./items.json", 'r+')
+  amounts = json.load(itemsfile)
+  itemsfile.close()
+
+# pr.register code for use
+#  id = ctx.message.author.id
+#  if str(id) not in amounts:
+#    currencyfile = open('./currency.json', 'r+')
+#    json.load(currencyfile)
+#    amounts[id] = str("100")
+#    json.dump(amounts, currencyfile)
+#    await ctx.send(embed=successEmbed)
+#    currencyfile.close()
+#    currencyfile = open('./currency.json', 'r')
+#    currencyfile.read()
+#    currencyfile.close()
+#  else:
+#    await ctx.send(embed=failiureEmbed)
+
+
+  
   shopEmbed.set_author(name="PyRobot", icon_url="https://cdn.discordapp.com/avatars/503024140706643968/6b57be03dc7ac21f337884fbbe4516de.webp")
   # embed.add_field(name="Voice Latency", value=str(VoiceClient.latency))
-  shopEmbed.add_field(name="Text Latency", value=str(int(bot.latency * 1000)) + 'ms')
-
+  for amount in amounts:
+    shopEmbed.add_field(name='`' + amounts[amount]['actualname'] + '`', value=str(amounts[amount]['friendlyname']) + '\n' + amounts[amount]['description'] + '\nCosts: ' + amounts[amount]['cost'], inline='false')
+    print('added ' + amounts[amount]['actualname'])
+  
   await ctx.send(embed=shopEmbed)
