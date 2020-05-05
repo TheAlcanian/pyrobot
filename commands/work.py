818550f83ef0734ecfa449a83ccf25f278ca1eea
TheAlcanian <IAmTheShardmaster@protonmail.com>
2020-05-04 19:28:02 -0700

    close #8

--- commands/work.py
+++ commands/work.py
@@ -4,28 +4,12 @@ async def work(ctx):
   amounts = json.load(currencyfile)
   currencyfile.close()
   currencyfile = open("./currency.json", 'w+')
-  print(amounts)
-  gainedcurrency = random.randint(100, 150)
-  addamount = int(amounts[str(ctx.message.author.id)]) + gainedcurrency
+  gainedcurrency = str(random.randint(100, 150))
+  addamount = int(amounts[str(ctx.message.author.id)]) + int(gainedcurrency)
   amounts[str(ctx.message.author.id)] = str(addamount)
   json.dump(amounts, currencyfile)
   currencyfile.close()
   await ctx.send('You worked at an unknown area and gained ' + str(gainedcurrency) + ' PyRolls!')
 
 
-
-# pr.register code for use
-#  id = ctx.message.author.id
-#  if str(id) not in amounts:
-#    currencyfile = open('./currency.json', 'r+')
-#    json.load(currencyfile)
-#    amounts[id] = str("100")
-#    json.dump(amounts, currencyfile)
-#    await ctx.send(embed=successEmbed)
-#    currencyfile.close()
-#    currencyfile = open('./currency.json', 'r')
-#    currencyfile.read()
-#    currencyfile.close()
-#  else:
-#    await ctx.send(embed=failiureEmbed)
 
